6237a7cd7db54d69b3ea4bc4096485920ab8a14d
Lastril <76162135+Lastril@users.noreply.github.com>
2021-11-22 15:00:07 +0100

    Elevator

--- src/main/java/fr/dut/jelevator/building/Building.java
+++ src/main/java/fr/dut/jelevator/building/Building.java
@@ -1,12 +1,19 @@
 package fr.dut.jelevator.building;
 
+import fr.dut.jelevator.elevator.Elevator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Building {
 
     private final int floor;
     private final double heightFloor;
     private int resident;
+    private final List<Elevator> elevatorList;
 
     public Building(int floor, int resident, double heightFloor) {
+        this.elevatorList = new ArrayList<>();
         this.floor = floor;
         this.resident = resident;
         this.heightFloor = heightFloor;
---
+++ src/main/java/fr/dut/jelevator/elevator/Elevator.java
@@ -0,0 +1,12 @@
+package fr.dut.jelevator.elevator;
+
+public class Elevator {
+
+    private double acceleration, maxSpeed, currentSpeed;
+
+    public Elevator() {
+
+    }
+
+
+}
