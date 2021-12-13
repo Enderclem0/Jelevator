48f1782ab2c9fb21d10662eaa6752e008f5b08f2
Enderclem0 <cduval07.u-pem.fr>
2021-12-13 14:55:41 +0100

    JSP

--- src/main/java/fr/dut/jelevator/Controller.java
+++ src/main/java/fr/dut/jelevator/Controller.java
@@ -9,6 +9,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static fr.dut.jelevator.building.BuildingFactory.getInstance;
 
@@ -19,40 +20,42 @@ public class Controller {
     private List<Building> buildings;
     private int drawX = 0;
     public void initialize() {
-        System.out.println("Initialize controller");
         gc = canvas.getGraphicsContext2D();
         buildings = new ArrayList<>();
         buildings.add(getInstance().setFloor(5).setHeightFloor(10).setResident(20).createNewBuilding());
-        buildings.add(getInstance().setFloor(3).setHeightFloor(10).setResident(20).createNewBuilding());
+        buildings.add(getInstance().setFloor(5).setHeightFloor(5).setResident(20).createNewBuilding());
         drawScene(10);
 
     }
     public void drawScene(int margin) {
         drawX = margin;
+        int lineWidth = 5;
         OptionalDouble oMaxHeight = buildings.stream().mapToDouble(Building::getHeight).max();
-        System.out.println("Getting building height...");
         double maxHeight = 0;
         if (oMaxHeight.isPresent()) {
             maxHeight = oMaxHeight.getAsDouble();
         }
-        System.out.println("Buildings size: " + buildings.size());
-        System.out.println("Canvas width: " + canvas.getWidth());
         double yUnit = canvas.getHeight() / maxHeight;
         double xUnit = (canvas.getWidth() / buildings.size()-(margin* buildings.size()+1));
-        System.out.println("xUnit value: " + xUnit);
         gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        gc.setLineWidth(5);
-        System.out.println("Drawing buildings...");
+        gc.setLineWidth(lineWidth);
         buildings.forEach(building -> {
             gc.setFill(getRandomBrightColor());
-            gc.rect(drawX, canvas.getHeight() - (canvas.getHeight()-building.getHeight()*yUnit), xUnit, building.getHeight() * yUnit);
+            System.out.println("drawing building : \n");
+            System.out.println("x : " + drawX + " y : " + (canvas.getHeight() - building.getHeight() * yUnit));
+            System.out.println("width : " + xUnit + " height : " + building.getHeight() * yUnit);
+            gc.rect(drawX, canvas.getHeight() - building.getHeight() * yUnit, xUnit, building.getHeight() * yUnit);
             gc.stroke();
             drawX += xUnit + margin;
-            System.out.println("Updating drawX: " + drawX);
         });
+    }
+    public void drawElevator(Elevator elevator) {
 
     }
     public Color getRandomBrightColor() {
-        return Color.hsb(Math.random() * 360, 0.75, 1);
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        Color rand = Color.hsb(random.nextInt(360), 0.75, 1);
+        System.out.println("Random color:"+rand+"\n");
+        return rand;
     }
 }
